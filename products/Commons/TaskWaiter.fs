e8895fa0ef95b522efb488387e5eb664393a0d5a
nabiki_t <29672245+nabiki-t@users.noreply.github.com>
2025-12-02 20:35:23 +0900

    Add: TaskWaiter class implementation.
    It is intended to be used as an initiator that sends and receives SCSI commands.

---
+++ products/Commons/TaskWaiter.fs
@@ -0,0 +1,182 @@
+﻿//=============================================================================
+// Haruka Software Storage.
+// TaskWaiter.fs : Definitions of TaskWaiter class
+//
+
+//=============================================================================
+// Namespace declaration
+
+namespace Haruka.Commons
+
+//=============================================================================
+// Import declaration
+
+open System
+open System.Threading
+open System.Threading.Tasks
+open System.Collections.Immutable
+
+//=============================================================================
+// Type definition
+
+/// Maintaining task state
+[<NoComparison; Struct>]
+type private TaskState<'TR> =
+    /// Indicates that a Wait request has been made but a Release has not been made.
+    | Wait of TaskCompletionSource<'TR>
+    /// Indicates that a Release request has been made but a Wait has not been made.
+    | Release of TaskCompletionSource<'TR>
+
+/// <summary>
+///  Implement a function to wait for task completion by specifying a number.
+/// </summary>
+/// <example>
+/// <code>
+/// <![CDATA[
+/// let w = TaskWaiter< int, int >()
+/// (* When Release is called after Wait, in which case the call to Wait will not return until Release is called. *)
+/// (* thread 1 *)
+/// let th1 = fun () -> task {
+///   let! res = w.Wait( 0 )
+///   printfn "result = %d" res
+/// }
+/// (* thread 1 *)
+/// let th2 = fun () -> task {
+///   Thread.Sleep 9999999
+///   w.Release( 0 )
+/// }
+/// 
+/// (* If Wait is called after Release, the Wait call will finish immediately. *)
+/// task {
+///   w.Release( 1 )
+///   let! res = w.Wait( 1 )
+///   printfn "result = %d" res
+/// }
+/// ]]>
+/// </code>
+/// </example>
+/// <remarks>
+///  TI specifies the data type to be used for the ID.
+///  TR specifies the data type that will be the processing result of the task.
+///  It is not assumed that Wait and Release will be performed multiple times simultaneously with the same ID specified.
+///  If Wait is executed twice with the same ID and then Release is called twice, 
+///  the first Release will release the locks of both threads that are waiting. 
+///  The second Release will register the ID, and that ID will remain thereafter.
+/// <code>
+/// <![CDATA[
+///   -------------------------------------------------------------------------> time
+///                            The locks on both thread 1 and 2 are released.
+///                            v
+///   thread 1 ====Wait........|===============================================
+///   thread 2 ==========Wait..|===============================================
+///   thread 3 ================Release=========================================
+///   thread 4 ==========================Release===============================
+///                                      ^
+///                                      A new ID will be registered.
+/// ]]>
+/// </code>
+/// If two Release calls are made with the same ID and then two Wait calls are made, the second Release call is ignored.
+/// The subsequent Wait call completes immediately, and the second Wait call continues to wait forever.
+/// <code>
+/// <![CDATA[
+///   -------------------------------------------------------------------------> time
+///   thread 1 ===Release======================================================
+///   thread 2 ============Release=============================================
+///                        ^
+///                        ignored   Completes immediately
+///                                  v
+///   thread 3 ======================Wait======================================
+///   thread 4 =============================Wait...............................
+///                                         ^
+///                                         Wait forever.
+/// ]]>
+/// </code>
+/// </remarks>
+type TaskWaiter<'TI, 'TR when 'TI : equality>() =
+
+    /// Keeping the generated tasks
+    let d = OptimisticLock( ImmutableDictionary< 'TI, TaskState<'TR> >.Empty )
+
+    /// <summary>
+    ///  Wait for task completion by specifying ID.
+    ///  If the ID has already been released, the task will immediately terminate.
+    /// </summary>
+    /// <param name="idx">
+    ///  Task ID.
+    /// </param>
+    /// <returns>
+    ///  Generated Task.
+    /// </returns>
+    member _.Wait ( idx : 'TI ) : Task< 'TR > =
+        let waitt =
+            d.Update( fun oldd ->
+                let r, v = oldd.TryGetValue( idx )
+                if r then
+                    match v with
+                    | TaskState.Wait( x ) ->
+                        struct( oldd, x )
+                    | TaskState.Release( x ) ->
+                        struct( oldd.Remove idx, x )
+                else
+                    let t = TaskCompletionSource<'TR>( TaskCreationOptions.RunContinuationsAsynchronously )
+                    let t2 = TaskState.Wait( t )
+                    struct( oldd.Add( idx, t2 ), t )
+            )
+        waitt.Task
+
+    /// <summary>
+    ///  Notify task completion by specifying ID.
+    /// </summary>
+    /// <param name="idx">
+    ///  Task ID.
+    /// </param>
+    /// <param name="res">
+    ///  Task execution result.
+    /// </param>
+    member this.Release ( idx : 'TI ) ( res : 'TR ) : unit =
+        match this.UpdateForRelease idx with
+        | ValueSome x ->
+            x.SetResult res
+        | _ ->
+            ()
+
+    /// <summary>
+    ///  Notify that an exception occurred in a task.
+    /// </summary>
+    /// <param name="idx">
+    ///  Task ID.
+    /// </param>
+    /// <param name="ex">
+    ///  The exception that occurred.
+    /// </param>
+    member this.SetException ( idx : 'TI ) ( ex : Exception ) : unit =
+        match this.UpdateForRelease idx with
+        | ValueSome x ->
+            x.SetException ex
+        | _ ->
+            ()
+
+    /// <summary>
+    ///  Update when task is completed.
+    /// </summary>
+    /// <param name="idx">
+    ///  Task ID.
+    /// </param>
+    member private _.UpdateForRelease ( idx : 'TI ) : TaskCompletionSource< 'TR > voption =
+        d.Update( fun oldd ->
+            let r, v = oldd.TryGetValue idx
+            if r then
+                match v with
+                | TaskState.Wait( x ) ->
+                    struct( oldd.Remove idx, ValueSome x )
+                | TaskState.Release( x ) ->
+                    struct( oldd, ValueNone )
+            else
+                let t =
+                    TaskCompletionSource<'TR>( TaskCreationOptions.RunContinuationsAsynchronously )
+                let t2 = TaskState.Release( t )
+                struct( oldd.Add( idx, t2 ), ValueSome t )
+        )
+
+    /// Registerd item count
+    member _.Count = d.obj.Count
---
+++ tests/UT_Common/TaskWaiterTest.fs
@@ -0,0 +1,195 @@
+﻿//=============================================================================
+// Haruka Software Storage.
+// TaskWaiterTest.fs : Test cases for TaskWaiter class.
+//
+
+//=============================================================================
+// Namespace declaration
+
+namespace Haruka.Test.UT.Commons
+
+//=============================================================================
+// Import declaration
+
+open System
+open System.Threading
+open System.Threading.Tasks
+
+open Xunit
+
+open Haruka.Commons
+
+//=============================================================================
+// Class implementation
+
+type TaskWaiter_Test() =
+
+    ///////////////////////////////////////////////////////////////////////////
+    // Test cases
+
+    [<Fact>]
+    member _.WR_001() =
+        let w = TaskWaiter<int, int>()
+        
+        [|
+            fun () -> task {
+                let! res = w.Wait( 0 )
+                Assert.True(( res = 99 ))
+            };
+            fun () -> task {
+                do! Task.Delay 500
+                w.Release 0 99
+            };
+        |]
+        |> Functions.RunTaskInPallalel
+        |> Functions.TaskIgnore
+
+    [<Fact>]
+    member _.WR_002() =
+        let w = TaskWaiter<int, int>()
+        
+        [|
+            fun () -> task {
+                let! res1 = w.Wait( 1 )
+                let! res2 = w.Wait( 2 )
+                let! res3 = w.Wait( 3 )
+                Assert.True(( res1 = 100 ))
+                Assert.True(( res2 = 200 ))
+                Assert.True(( res3 = 300 ))
+            };
+            fun () -> task {
+                do! Task.Delay 500
+                w.Release 1 100
+                w.Release 2 200
+                w.Release 3 300
+            };
+        |]
+        |> Functions.RunTaskInPallalel
+        |> Functions.TaskIgnore
+
+    [<Fact>]
+    member _.WE_001() =
+        let w = TaskWaiter<int, int>()
+        
+        [|
+            fun () -> task {
+                try
+                    let! _ = w.Wait( 0 )
+                    Assert.Fail __LINE__
+                with
+                | :? ArgumentException as x ->
+                    Assert.True(( x.Message = "aaaa" ))
+            };
+            fun () -> task {
+                do! Task.Delay 500
+                w.SetException 0 ( ArgumentException "aaaa" )
+            };
+        |]
+        |> Functions.RunTaskInPallalel
+        |> Functions.TaskIgnore
+
+    [<Fact>]
+    member _.WE_002() =
+        let w = TaskWaiter<int, int>()
+        
+        [|
+            fun () -> task {
+                try
+                    let! _ = w.Wait( 1 )
+                    Assert.Fail __LINE__
+                with
+                | :? ArgumentException as x ->
+                    Assert.True(( x.Message = "1111" ))
+
+                try
+                    let! _ = w.Wait( 2 )
+                    Assert.Fail __LINE__
+                with
+                | :? ArgumentException as x ->
+                    Assert.True(( x.Message = "2222" ))
+
+                try
+                    let! _ = w.Wait( 3 )
+                    Assert.Fail __LINE__
+                with
+                | :? ArgumentException as x ->
+                    Assert.True(( x.Message = "3333" ))
+
+            };
+            fun () -> task {
+                do! Task.Delay 500
+                w.SetException 1 ( ArgumentException "1111" )
+                w.SetException 2 ( ArgumentException "2222" )
+                w.SetException 3 ( ArgumentException "3333" )
+            };
+        |]
+        |> Functions.RunTaskInPallalel
+        |> Functions.TaskIgnore
+
+    [<Fact>]
+    member _.RW_001() =
+        task {
+            let w = TaskWaiter<int, int>()
+            w.Release 1 100
+            let! res1 = w.Wait( 1 )
+            Assert.True(( res1 = 100 ))
+        }
+
+    [<Fact>]
+    member _.RW_002() =
+        task {
+            let w = TaskWaiter<int, int>()
+            w.Release 1 100
+            w.Release 2 200
+            w.Release 3 300
+            let! res1 = w.Wait( 1 )
+            let! res2 = w.Wait( 2 )
+            let! res3 = w.Wait( 3 )
+            Assert.True(( res1 = 100 ))
+            Assert.True(( res2 = 200 ))
+            Assert.True(( res3 = 300 ))
+        }
+
+    [<Fact>]
+    member _.EW_001() =
+        task {
+            let w = TaskWaiter<int, int>()
+            w.SetException 0 ( ArgumentException "aaaa" )
+            try
+                let! _ = w.Wait( 0 )
+                Assert.Fail __LINE__
+            with
+            | :? ArgumentException as x ->
+                Assert.True(( x.Message = "aaaa" ))
+        }
+
+    [<Fact>]
+    member _.EW_002() =
+        task {
+            let w = TaskWaiter<int, int>()
+            w.SetException 1 ( ArgumentException "1111" )
+            w.SetException 2 ( ArgumentException "2222" )
+            w.SetException 3 ( ArgumentException "3333" )
+
+            try
+                let! _ = w.Wait( 1 )
+                Assert.Fail __LINE__
+            with
+            | :? ArgumentException as x ->
+                Assert.True(( x.Message = "1111" ))
+
+            try
+                let! _ = w.Wait( 2 )
+                Assert.Fail __LINE__
+            with
+            | :? ArgumentException as x ->
+                Assert.True(( x.Message = "2222" ))
+
+            try
+                let! _ = w.Wait( 3 )
+                Assert.Fail __LINE__
+            with
+            | :? ArgumentException as x ->
+                Assert.True(( x.Message = "3333" ))
+        }
+
